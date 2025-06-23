1f487a9822972306de163d3f145e80758da52214
IvanRL-11 <ivanromerol2002@gmail.com>
2025-06-23 17:11:35 -0600

    Menú movil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,49 @@
+import { useState } from "react";
 import Hero from "./Hero";
 import ParticlesBg from "./ParticlesBg";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <>
       <ParticlesBg />
       <header>
-        <nav className="menu-nav cont-margin">
+        <nav className="menu-nav">
           <div className="logo">
             <a href="../index.html">
               <img src="/vite.svg" alt="logo-website" />
             </a>
           </div>
-          <div className="sections-nav">
-            <a className="hover-underline" href="#skills">
+
+          <button
+            className={`menu-toggle ${menuOpen ? "open" : ""}`}
+            onClick={toggleMenu}
+            aria-label="Abrir/Cerrar menú"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+          <div className={`sections-nav ${menuOpen ? "open" : ""}`}>
+            <a href="#skills" onClick={closeMenu} className="hover-underline">
               Habilidades
             </a>
-            <a className="hover-underline" href="#projects">
+            <a href="#projects" onClick={closeMenu} className="hover-underline">
               Proyectos
             </a>
-            <a className="hover-underline" href="#contact">
+            <a href="#contact" onClick={closeMenu} className="hover-underline">
               Contacto
             </a>
           </div>
         </nav>
       </header>
+
+      {menuOpen && <div className="overlay" onClick={closeMenu}></div>}
+
       <Hero />
     </>
   );
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Message = ({msg, bgColor}) => {
-    let styles = {
-        padding: "1rem",
-        marginBottom: "1rem",
-        textAlign: "center",
-        color: "#fff",
-        fontWeight: "bold",
-        backgroundColor: bgColor,
-    }
+const Message = ({ msg, bgColor }) => {
+  let styles = {
+    padding: "0.8rem",
+    marginBottom: "0.8rem",
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    backgroundColor: "#00cc66",
+    fontFamily: "'Inter', sans-serif",
+  };
   return (
     <div style={styles}>
       <p>{msg}</p>
