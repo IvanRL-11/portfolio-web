8fa6c67e176c889f937acf10d5bf59cac7834022
IvanRL-11 <ivanromerol2002@gmail.com>
2025-07-06 15:12:15 -0600

    Ajuste particulas Móvil

--- src/components/ParticlesBg.jsx
+++ src/components/ParticlesBg.jsx
@@ -2,17 +2,17 @@ import Particles from "./Particles";
 import "../Styles/Particles.css";
 
 const ParticlesBg = () => {
+  const isMobile = window.innerWidth < 768;
+
   return (
-    <div
-      className="particles-bg" /*style={{ width: "100%", height: "600px", position: "relative" }}*/
-    >
+    <div className="particles-bg">
       <Particles
         particleColors={["#00CC66", "#00CC66"]}
-        particleCount={300}
-        particleSpread={10}
-        speed={0.1}
-        particleBaseSize={100}
-        moveParticlesOnHover={true}
+        particleCount={isMobile ? 80 : 300}
+        particleSpread={isMobile ? 30 : 10}
+        speed={isMobile ? 0.05 : 0.1}
+        particleBaseSize={isMobile ? 20 : 100}
+        moveParticlesOnHover={!isMobile}
         alphaParticles={false}
         disableRotation={false}
       />
