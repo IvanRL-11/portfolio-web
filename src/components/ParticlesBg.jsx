6f93e5c69df9fbb41c5eec4c87f21bc1a36ed5d2
IvanRL-11 <ivanromerol2002@gmail.com>
2025-07-06 15:30:29 -0600

    Ajuste transición de btns

--- src/components/ParticlesBg.jsx
+++ src/components/ParticlesBg.jsx
@@ -2,16 +2,16 @@ import Particles from "./Particles";
 import "../Styles/Particles.css";
 
 const ParticlesBg = () => {
-  const isMobile = window.innerWidth < 768;
+   const isMobile = window.innerWidth < 768;
 
   return (
     <div className="particles-bg">
       <Particles
         particleColors={["#00CC66", "#00CC66"]}
-        particleCount={isMobile ? 80 : 300}
-        particleSpread={isMobile ? 30 : 10}
-        speed={isMobile ? 0.05 : 0.1}
-        particleBaseSize={isMobile ? 20 : 100}
+        particleCount={isMobile ? 150 : 300}
+        particleSpread={isMobile ? 15 : 10}
+        speed={isMobile ? 0.07 : 0.1}
+        particleBaseSize={isMobile ? 100 : 100}
         moveParticlesOnHover={!isMobile}
         alphaParticles={false}
         disableRotation={false}
